f323139fb7770f426485dbeee5aec06c4ee25e51
sahil5505 <sahilrevankar20co21@gmail.com>
2024-08-09 17:29:59 +0530

    some changes

--- client/src/app/dashboard/CardPopularProducts.tsx
+++ client/src/app/dashboard/CardPopularProducts.tsx
@@ -1,26 +1,52 @@
-import { useGetDashboardMetricsQuery } from '@/state/api'
-import React from 'react'
-
+import { useGetDashboardMetricsQuery } from "@/state/api";
+import React from "react";
 
 const CardPopularProducts = () => {
-
-    const {data: DashboardMetrics, isLoading} = useGetDashboardMetricsQuery();
-
+  const { data: dashboardMetrics, isLoading } = useGetDashboardMetricsQuery();
 
   return (
-    <div className='row-span-3 xl:row-span-6 bg-white shadow-md rounded-2xl pb-16'>
-        {isLoading ? (
-            <div className='n-5'>Loading...</div>
-        ) : (
-            <>
-            <h3 className='text-lg font-semibold px-7 pt-5 pb-2'>
-                Popular Product 
-            </h3>
-            </>
-        )}
-
+    <div className="row-span-3 xl:row-span-6 bg-white shadow-md rounded-2xl pb-16">
+      {isLoading ? (
+        <div className="m-5">Loading...</div>
+      ) : (
+        <>
+          <h3 className="text-lg font-semibold px-7 pt-5 pb-2">
+            Popular Products
+          </h3>
+          <hr />
+          <div className="overflow-auto h-full">
+            {dashboardMetrics?.popularProducts.map((product) => (
+              <div
+                key={product.productId}
+                className="flex items-center justify-between gap-3 px-5 py-7 border-b"
+              >
+                <div>
+                  Image
+               </div>
+                   
+                  <div className="flex flex-col justify-between gap-1">
+                    <div className="font-bold text-gray-700">
+                      {product.name}
+                    </div>
+                  <div className="flex text-sm items-center">
+                    <span className="font-bold text-blue-500 text-xs">
+                        ${product.price}    
+                    </span>
+                    <span className="mx-2">
+                        |
+                    </span>
+                    <div>
+                        rating
+                    </div>
+                  </div>
+                  </div>
+                </div>
+            ))}
+          </div>
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default CardPopularProducts
+export default CardPopularProducts;
